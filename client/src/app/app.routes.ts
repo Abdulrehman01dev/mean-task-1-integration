e9207eb5d821418d534f94fb4eeb871b48d83751
Abdulrehman01dev <hunaindev1@gmail.com>
2025-10-29 16:50:02 +0500

    Add Angular Material and CDK dependencies WITH Azure Blue theme, and update styles. Enhance index.html with Google Fonts and Material Icons (Auto done my ng). Modify app.component.html for GitHub integration and set up routing for integrations. Also added a lazy load module called integration with Github compoenet.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'integrations',
+    loadChildren: () => import('./integrations/integrations.routes').then(m => m.INTEGRATIONS_ROUTES)
+  }
+];
---
+++ client/src/app/integrations/integrations.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { GithubComponent } from './github/github.component';
+
+export const INTEGRATIONS_ROUTES: Routes = [
+  {
+    path: 'github',
+    component: GithubComponent
+  }
+];
+
+
